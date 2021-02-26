8cb1e78756c6b49eda3867da95ea42cb59864ecd
zoshigayan <hello@zoshigayan.net>
2021-02-27 04:22:46 +0900

    詳細つくったよ

--- controllers/book.go
+++ controllers/book.go
@@ -11,10 +11,12 @@ type BookController struct {
 
 func (b BookController) Init(g *echo.Group) {
 	g.GET("", b.Index)
+	g.GET("/:id", b.Show)
 }
 
 func (b BookController) Index(c echo.Context) error {
 	books := []struct {
+		Id          string
 		Thumbnail   string
 		Title       string
 		Subtitle    string
@@ -23,6 +25,7 @@ func (b BookController) Index(c echo.Context) error {
 		Description string
 	}{
 		{
+			"hoge",
 			"https://placehold.it/128x128?text=hoge",
 			"ほげ",
 			"ふが",
@@ -33,5 +36,28 @@ func (b BookController) Index(c echo.Context) error {
 	}
 	return c.Render(http.StatusOK, "book/index", map[string]interface{}{
 		"Books": books,
+	})
+}
+
+func (b BookController) Show(c echo.Context) error {
+	book := struct {
+		Id          string
+		Thumbnail   string
+		Title       string
+		Subtitle    string
+		Authors     []string
+		PublishDate time.Time
+		Description string
+	}{
+		"hoge",
+		"https://placehold.it/128x128?text=hoge",
+		"ほげ",
+		"ふが",
+		[]string{"太郎", "花子"},
+		time.Now(),
+		"名著だよ～",
+	}
+	return c.Render(http.StatusOK, "book/show", map[string]interface{}{
+		"Book": book,
 	})
 }
